412bb705d0e865d5610c9b6e02ed8068f3c38073
Abhishek Verma <66030096+Abhi8787@users.noreply.github.com>
2022-06-09 13:42:33 +0530

    Create solution.java

---
+++ Day1 : Arrays/3. Next Permutation/solution.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        int j = nums.length-2;
+        
+        while(j>=0)
+        {
+            if(nums[j] < nums[j+1])
+                break;
+            else
+                j--;
+        }
+        
+        int temp = j;
+        
+        for(int i=nums.length-1 ; i>temp && temp>=0 ; i--)
+        {
+            
+            if(nums[i] > nums[temp])
+            {
+
+                int temp1 = nums[temp];
+                nums[temp] = nums[i];
+                nums[i] = temp1;
+                
+                break;
+            }
+            
+        }
+        
+
+        int i = temp+1;
+        j = nums.length-1;
+        
+        while(i<j)
+        {
+            int temp1 = nums[i];
+            nums[i] = nums[j];
+            nums[j]= temp1;
+            
+            i++;
+            j--;
+        }
+        
+    }
+    
+    // [5,1,3,4,5,6,4,3,45,3,4,76,7,1]  --> Test Case
+            
+}
